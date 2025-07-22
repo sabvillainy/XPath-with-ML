70381c6fe4100a9970dc8969975329de1833d7d4
Sabri Yılmaz <sabri6yilmaz@gmail.com>
2025-07-22 09:34:50 +0300

    add basic xpath extraction example

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("XPath with ML")
+from lxml import html
+
+html_code = """
+<html>
+  <body>
+    <div class="product">
+      <span class="price">$19.99</span>
+    </div>
+  </body>
+</html>
+"""
+
+tree = html.fromstring(html_code)
+price = tree.xpath("//span[@class='price']/text()")[0]
+print(price)
